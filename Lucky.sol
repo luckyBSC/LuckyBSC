d12872c9d30770e99693ddd2294d74520e00b985
luckyBSC <luckytokenbsc@protonmail.com>
2021-06-15 13:28:50 -0400

    _getRate

--- Lucky.sol
+++ Lucky.sol
@@ -976,7 +976,7 @@ contract Lucky is Context, IERC20, Ownable {
         for (uint256 i = 0; i < _excluded.length; i++) {
             if (_excluded[i] == account) {
                 _excluded[i] = _excluded[_excluded.length - 1];
-                uint256 currentRate = getRate();
+                uint256 currentRate = _getRate();
                 _rOwned[account] = _tOwned[account].mul(currentRate);
                 _tOwned[account] = 0;
                 _isExcluded[account] = false;
